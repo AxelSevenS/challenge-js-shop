8ab7c8818c9f37d28704904b9a05c91e308de831
AxelSeven <spaghettiandmeatballs61@gmail.com>
2022-06-10 23:14:32 +0200

    discount system

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const items = {
-    "computer": 5,
-    "iphone13": 999999,
-    "axel": 1000,
-    "cristian": 10,
-    "quentin": 50,
-    "guillaume": 2
+    "computer": ["Ordinateur", 5, 35],
+    "iphone13": ["IPhone 13", 999999, 15],
+    "axel": ["Axel Ga&euml;tan Robert Sevenet", 1000, 0],
+    "cristian": ["Cristian", 10, 75],
+    "quentin": ["Quentin Dofus", 50, 5],
+    "guillaume": ["Guillaume", 2, 0]
 }
 let shop;
 let cart;
@@ -42,7 +42,7 @@ function removeAllFromCart(){
     window.location.reload(true);
 }
 
-function setItemPrice(itemName, itemPrice){ 
+function setItemPrice(itemName, itemPrice, itemDiscount){ 
     let cartItem = document.getElementById(`${itemName}--cart`);
     
     if (localStorage.getItem(itemName) == null){
@@ -50,11 +50,12 @@ function setItemPrice(itemName, itemPrice){
     }
     const itemQuantity = localStorage.getItem(itemName);
     const totalPrice = itemPrice * itemQuantity;
+    const finalPrice = totalPrice - (totalPrice * itemDiscount / 100);
 
     console.log(itemName, itemQuantity, totalPrice);
 
-    cartItem.querySelectorAll(".item-price")[0].innerHTML = `${itemPrice}euros * ${itemQuantity}`;
-    cartItem.querySelectorAll(".item-total")[0].innerHTML = `${totalPrice}euros`;
+    cartItem.querySelector(".item-price").innerHTML = `${itemPrice}&euro; * ${itemQuantity}`;
+    cartItem.querySelector(".item-total").innerHTML = `${totalPrice}&euro; - ${itemDiscount}% = ${finalPrice}&euro;`;
 
     if (itemQuantity == 0){
         cartItem.style.display = "none";
@@ -62,16 +63,17 @@ function setItemPrice(itemName, itemPrice){
         cartItem.style.display = "";
     }
 
-    return totalPrice;
+    return finalPrice;
 }
 function setAllItemPrices(){
     let total = 0;
     for (let itemName in items){
-        const itemPrice = items[itemName];
+        const itemPrice = items[itemName][1];
+        const itemDiscount = items[itemName][2];
 
-        total += setItemPrice(itemName, itemPrice);
+        total += setItemPrice(itemName, itemPrice, itemDiscount);
     }
-    document.getElementById('total-price').innerHTML = `Total des Achats en Cours vaut : ${total}euros`;
+    document.getElementById('total-price').innerHTML = `Prix Total : ${total}&euro;`;
 }
 function toggleCart(){
 
@@ -91,12 +93,14 @@ document.addEventListener("DOMContentLoaded", function() {
     let shopList = document.getElementById("shop-list");
     const shopTemplate = document.getElementById("TEMPLATE--shop");
     for (let itemName in items){
-        const itemPrice = items[itemName];
+        const properName = items[itemName][0];
+        const itemPrice = items[itemName][1];
         let itemTemplate = shopTemplate.cloneNode(true);
         itemTemplate.id = `${itemName}--shop`;
         itemTemplate.style = "";
         itemTemplate.innerHTML = itemTemplate.innerHTML.replaceAll("TEMPLATE", itemName);
-        itemTemplate.querySelectorAll(".item-price")[0].innerHTML = `${itemPrice}euros`;
+        itemTemplate.innerHTML = itemTemplate.innerHTML.replaceAll("NAME", properName);
+        itemTemplate.querySelectorAll(".item-price")[0].innerHTML = `${itemPrice}&euro;`;
         shopList.appendChild(itemTemplate);
     }
     shopTemplate.remove();
@@ -104,10 +108,12 @@ document.addEventListener("DOMContentLoaded", function() {
     let cartList = document.getElementById("cart-list");
     const cartTemplate = document.getElementById("TEMPLATE--cart");
     for (let itemName in items){
+        const properName = items[itemName][0];
         let itemTemplate = cartTemplate.cloneNode(true);
         itemTemplate.id = `${itemName}--cart`;
         itemTemplate.style = "";
         itemTemplate.innerHTML = itemTemplate.innerHTML.replaceAll("TEMPLATE", itemName);
+        itemTemplate.innerHTML = itemTemplate.innerHTML.replaceAll("NAME", properName);
         cartList.appendChild(itemTemplate);
     }
     cartTemplate.remove();
